067fce8e61705054e284e9205dfd85be17b55c06
Pertaub <arjoonsing.pertaub@accenture.com>
2018-07-11 15:55:57 +0400

    version2

--- src/main/java/com/accenture/devops/HelloArjoonProjectApplication.java
+++ src/main/java/com/accenture/devops/HelloArjoonProjectApplication.java
@@ -2,9 +2,10 @@ package com.accenture.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloArjoonProjectApplication {
+public class HelloArjoonProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloArjoonProjectApplication.class, args);
